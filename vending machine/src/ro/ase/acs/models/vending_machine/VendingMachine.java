03c63d306af5f327b5d373150ac6bf59e7267f70
costinvlad <vladnicolae22@stud.ase.ro>
2025-03-24 19:26:54 +0200

    updated vending machine class

--- vending machine/src/ro/ase/acs/models/vending_machine/VendingMachine.java
+++ vending machine/src/ro/ase/acs/models/vending_machine/VendingMachine.java
@@ -1,6 +1,7 @@
 package ro.ase.acs.models.vending_machine;
 
 import ro.ase.acs.models.compartment.Compartment;
+import ro.ase.acs.utils.Static;
 
 public class VendingMachine {
     private static int idCounter = 0;
@@ -29,7 +30,8 @@ public class VendingMachine {
     }
 
     public void setName(String name) {
-        this.name = name;
+        if (!name.isBlank())
+            this.name = name;
     }
 
     public String getLocation() {
@@ -37,7 +39,8 @@ public class VendingMachine {
     }
 
     public void setLocation(String location) {
-        this.location = location;
+        if (!location.isBlank())
+            this.location = location;
     }
 
     public Compartment getCompartment() {
@@ -46,5 +49,54 @@ public class VendingMachine {
 
     public void setCompartment(Compartment compartment) {
         this.compartment = compartment;
+    }
+
+    public static VendingMachine getVendingMachine(int id) {
+        for (VendingMachine vm : Static.vendingMachines) {
+            if (vm.getId() == id) {
+                return vm;
+            }
+        }
+        return null;
+    }
+
+    public static void addVendingMachine(VendingMachine vendingMachine) {
+        Static.vendingMachines.add(vendingMachine);
+        System.out.println("Vending Machine " + vendingMachine + " added to list.");
+    }
+
+    public static void updateVendingMachine(int id, String name, String location) {
+        VendingMachine vm = VendingMachine.getVendingMachine(id);
+        if (vm != null) {
+            vm.setName(name);
+            vm.setLocation(location);
+            System.out.println("Vending Machine updated...");
+        } else {
+            System.out.println("Vending Machine not found.");
+        }
+    }
+
+    public static void deleteVendingMachine(int id) {
+        VendingMachine vm = VendingMachine.getVendingMachine(id);
+        if (vm != null) {
+            Static.vendingMachines.remove(vm);
+            System.out.println("Vending Machine removed...");
+        } else {
+            System.out.println("Vending Machine not found.");
+        }
+    }
+
+    public static void listVendingMachines() {
+        System.out.println(Static.vendingMachines);
+    }
+
+    @Override
+    public String toString() {
+        return "VendingMachine{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", location='" + location + '\'' +
+                ", compartment=" + compartment +
+                '}';
     }
 }
